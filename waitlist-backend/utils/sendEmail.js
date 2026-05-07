45fc48ab7ebe5fead595bd991f2a77e01947c68f
Poojanpatel2003 <poojanpatel1595@gmail.com>
2026-05-07 23:19:22 +0530

    Emaijs

--- waitlist-backend/utils/sendEmail.js
+++ waitlist-backend/utils/sendEmail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async (to, name) => {
   try {
     const data = await resend.emails.send({
-      from: "poojanpatel1595@gmail.com",
+      from: "onboarding@resend.dev",
 
       to,
 
