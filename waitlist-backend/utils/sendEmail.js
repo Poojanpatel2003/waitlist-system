eb9e6e1cee36e4d1f51b92ae877dabe5668e78fb
Poojanpatel2003 <poojanpatel1595@gmail.com>
2026-05-07 17:21:00 +0530

    email check

--- waitlist-backend/utils/sendEmail.js
+++ waitlist-backend/utils/sendEmail.js
@@ -1,6 +1,11 @@
 import nodemailer from "nodemailer";
 
 const sendEmail = async (to, name) => {
+  console.log("EMAIL FUNCTION STARTED");
+
+  console.log("EMAIL_USER:", process.env.EMAIL_USER);
+  console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "EXISTS" : "MISSING");
+
   try {
 
     const transporter = nodemailer.createTransport({
@@ -11,56 +16,25 @@ const sendEmail = async (to, name) => {
       },
     });
 
-   
+    console.log("TRANSPORT CREATED");
+
     await transporter.verify();
-    console.log("Transport Ready");
 
-    const htmlTemplate = `
-      <div style="font-family: Arial, sans-serif; background-color: #f4f6f8; padding: 20px;">
-        
-        <div style="max-width: 500px; margin: auto; background: #ffffff; border-radius: 10px; overflow: hidden; box-shadow: 0 4px 10px rgba(0,0,0,0.1);">
-          
-          <div style="background: linear-gradient(135deg, #4f46e5, #3b82f6); padding: 20px; text-align: center;">
-            <h2 style="color: #ffffff; margin: 0;">Welcome 🚀</h2>
-          </div>
+    console.log("SMTP VERIFIED");
 
-          <div style="padding: 25px;">
-            <h3 style="margin-top: 0;">Hello ${name},</h3>
-            
-            <p style="color: #555; line-height: 1.5;">
-              Thank you for joining our waitlist 🎉.
-              We will notify you soon.
-            </p>
-
-            <p style="color: #555; line-height: 1.5;">
-              We’re excited to have you on board.
-            </p>
-
-            <div style="margin-top: 30px;">
-              <p style="margin: 0; color: #777;">
-                Best Regards,
-              </p>
-
-              <p style="margin: 5px 0 0; font-weight: bold; color: #111;">
-                Team Truvixoo
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    `;
-
-    await transporter.sendMail({
-      from: `"Poojan Patel" <${process.env.EMAIL_USER}>`,
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_USER,
       to,
-      subject: "🎉 You're on the Waitlist!",
-      html: htmlTemplate,
+      subject: "Test Email",
+      text: `Hello ${name}`,
     });
 
-    console.log("Email Sent Successfully");
+    console.log("EMAIL SENT");
+    console.log(info);
 
   } catch (error) {
-    console.error("Email Error:", error);
+    console.log("FULL EMAIL ERROR BELOW");
+    console.log(error);
   }
 };
 
