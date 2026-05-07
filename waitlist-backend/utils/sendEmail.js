dcd9ee956fa487c036dc7a9ced0e288b122cce57
Poojanpatel2003 <poojanpatel1595@gmail.com>
2026-05-07 22:43:44 +0530

    env change

--- waitlist-backend/utils/sendEmail.js
+++ waitlist-backend/utils/sendEmail.js
@@ -9,13 +9,13 @@ const sendEmail = async (to, name) => {
       service: "gmail",
 
       auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
     const info = await transporter.sendMail({
-      from: process.env.SMTP_USER,
+      from: process.env.EMAIL_USER,
 
       to,
 
