535eb0668340513b4a5d4aab9fe8c10ad36a42f1
Poojanpatel2003 <poojanpatel1595@gmail.com>
2026-05-07 17:28:53 +0530

    fixing cors

--- waitlist-backend/controllers/waitlistController.js
+++ waitlist-backend/controllers/waitlistController.js
@@ -3,14 +3,21 @@ import sendEmail from "../utils/sendEmail.js";
 
 export const addToWaitlist = async (req, res) => {
   try {
+
     const { name, email, phone, message } = req.body;
+
     if (!name || !email || !message) {
-      return res.status(400).json({ message: "All required fields missing" });
+      return res.status(400).json({
+        message: "All required fields missing",
+      });
     }
 
     const existingUser = await Waitlist.findOne({ email });
+
     if (existingUser) {
-      return res.status(400).json({ message: "Email already exists" });
+      return res.status(400).json({
+        message: "Email already exists",
+      });
     }
 
     const user = await Waitlist.create({
@@ -20,17 +27,26 @@ export const addToWaitlist = async (req, res) => {
       message,
     });
 
-     try {
-      await sendEmail(email, name);
-    } catch (err) {
-      console.log("Email Error:", err);
-    }
-    return res.status(201).json({
+    // Send response FIRST
+    res.status(201).json({
       message: "Successfully added to waitlist",
       user,
     });
 
+    // THEN send email in background
+    sendEmail(email, name)
+      .then(() => {
+        console.log("Email Sent");
+      })
+      .catch((err) => {
+        console.log("Email Error:", err);
+      });
+
   } catch (error) {
-    return res.status(500).json({ message: error.message });
+
+    return res.status(500).json({
+      message: error.message,
+    });
+
   }
 };
